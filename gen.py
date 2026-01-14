76534849adb273be63bc4549019f32df10ae6284
Oli <OliverWDGarrett@gmail.com>
2026-01-15 02:04:53 +1300

    working

--- gen.py
+++ gen.py
@@ -2,6 +2,7 @@ import os
 import re
 import requests
 from bs4 import BeautifulSoup
+from PIL import Image
 
 
 def sanitize(name):
@@ -9,7 +10,9 @@ def sanitize(name):
 
 
 def get_app_id(url):
-    return re.search(r'/app/(\d+)', url).group(1)
+    res = re.search(r'/app/(\d+)', url)
+    assert res
+    return res.group(1)
 
 
 def scrape_game_name(url):
@@ -33,6 +36,54 @@ def download_capsule(app_id, path):
             return
 
 
+def scale_and_crop(img, target_w, target_h):
+    ratio = target_h / img.height
+    new_w = int(img.width * ratio)
+    img = img.resize((new_w, target_h), Image.LANCZOS)
+    left = (new_w - target_w) // 2
+    return img.crop((left, 0, left + target_w, target_h))
+
+
+def make_outlined_cross(cross_img, size):
+    OUTLINE_WIDTH = 12
+    cross = cross_img.resize((size, size), Image.LANCZOS)
+
+    # Make black version
+    r, g, b, a = cross.split()
+    black = Image.merge('RGBA', (a.point(lambda x: 0), a.point(lambda x: 0), a.point(lambda x: 0), a))
+
+    # Canvas with padding for offsets
+    result = Image.new('RGBA', (size, size), (0, 0, 0, 0))
+
+    # 8 directions for outline
+    for dx, dy in [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]:
+        result.paste(black, (dx * OUTLINE_WIDTH, dy * OUTLINE_WIDTH), black)
+
+    # White cross on top
+    result.paste(cross, (0, 0), cross)
+    return result
+
+
+def create_package_header(folder):
+    game1 = Image.open(f"{folder}/game1.png")
+    game2 = Image.open(f"{folder}/game2.png")
+    cross = Image.open("cross.png").convert("RGBA")
+
+    out_w, out_h = 1414, 464
+    game_w = out_w // 2
+
+    cross = make_outlined_cross(cross, out_h)
+    g1 = scale_and_crop(game1, game_w, out_h)
+    g2 = scale_and_crop(game2, game_w, out_h)
+
+    result = Image.new('RGB', (out_w, out_h))
+    result.paste(g1, (0, 0))
+    result.paste(g2, (game_w, 0))
+    cross_x = (out_w - cross.width) // 2
+    result.paste(cross, (cross_x, 0), cross)
+    result.save(f"{folder}/package_header.png")
+
+
 def gen(url1, url2):
     game1, game2 = scrape_game_name(url1), scrape_game_name(url2)
     print(f"Game 1: {game1}\nGame 2: {game2}")
@@ -44,6 +95,9 @@ def gen(url1, url2):
     download_capsule(get_app_id(url1), f"{folder}/game1.png")
     download_capsule(get_app_id(url2), f"{folder}/game2.png")
     print("Downloaded capsules")
+
+    create_package_header(folder)
+    print("Created package_header.png")
 
 
 
