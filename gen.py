8894192d5f2e83ea57bb9f3c1f2fb82867529cdd
Oli <OliverWDGarrett@gmail.com>
2026-08-24 14:14:59 +1200

    zoominoes

--- gen.py
+++ gen.py
@@ -40,42 +40,17 @@ def scale_and_crop(img, target_w, target_h):
     return img.crop((left, 0, left + target_w, target_h))
 
 
-def make_outlined_cross(cross_img, size):
-    OUTLINE_WIDTH = min(12, size//20)
-    cross = cross_img.resize((size, size), Image.LANCZOS)
-
-    # Make black version
-    r, g, b, a = cross.split()
-    black = Image.merge('RGBA', (a.point(lambda x: 0), a.point(lambda x: 0), a.point(lambda x: 0), a))
-
-    # Canvas with padding for offsets
-    result = Image.new('RGBA', (size, size), (0, 0, 0, 0))
-
-    # 8 directions for outline
-    for dx, dy in [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]:
-        result.paste(black, (dx * OUTLINE_WIDTH, dy * OUTLINE_WIDTH), black)
-
-    # White cross on top
-    result.paste(cross, (0, 0), cross)
-    return result
-
-
 def create_bundle_image(folder, out_w, out_h, name):
     game1 = Image.open(f"{folder}/game1.png")
     game2 = Image.open(f"{folder}/game2.png")
-    cross = Image.open("cross.png").convert("RGBA")
 
     game_w = out_w // 2
-    cross = make_outlined_cross(cross, out_h // 2)
     g1 = scale_and_crop(game1, game_w, out_h)
-    g2 = scale_and_crop(game2, game_w, out_h)
+    g2 = scale_and_crop(game2, out_w - game_w, out_h)
 
     result = Image.new('RGB', (out_w, out_h))
     result.paste(g1, (0, 0))
     result.paste(g2, (game_w, 0))
-    cross_x = (out_w - cross.width) // 2
-    cross_y = (out_h - cross.height) // 2
-    result.paste(cross, (cross_x, cross_y), cross)
     result.save(f"{folder}/{name}.png")
 
 
@@ -99,11 +74,17 @@ def gen(url1, url2):
 
 
 
+
+LOOTPLOT = "https://store.steampowered.com/app/3057190/LOOTPLOT/"
+CATX11 = "https://store.steampowered.com/app/4173020/CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT/",
+
+
 # In future, other programs will call `generate`.
 # So please leave this API as is; don't add `input()` or anything weird.
 gen(
-    "https://store.steampowered.com/app/4173020/CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT_CAT/",
-    "https://store.steampowered.com/app/4615010/Kibble_Cats/"
+    LOOTPLOT,
+    "https://store.steampowered.com/app/3282420/Zoominoes/"
 )
+
 
 
